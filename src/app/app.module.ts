a1ad44b456b7764f5d7d8b33d281d4b0eaaa17ce
filipbarak <filipbarak94@gmail.com>
2017-07-13 14:58:13 +0200

    push

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,22 +6,8 @@ import {EhrFormComponentsModule, EhrFormModelModule, EhrFormService, Terminology
 // import EhrFormCustomComponentsModule for custom components
 import {AppComponent} from './app.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {BmiSimpleComponent} from "./bmi-simple/bmi-simple.component";
 import {BasicService} from "./basic.service";
-import {TempCalculatorComponent} from './temp-calculator/temp-calculator.component';
-import {Das28Component} from './das28/das28.component';
-import {MainDiagnosisComponent} from './main-diagnosis-component/main-diagnosis-component.component';
-import {GeneralStatusComponent} from './general-status/general-status.component';
-import {AllergiesComponent} from './allergies/allergies.component';
-import {SocialAnamesisComponent} from './social-anamesis/social-anamesis.component';
-import {AssociatedDiseasesComponent} from './associated-diseases/associated-diseases.component';
-import {ProceduresComponent} from './procedures/procedures.component';
-import {AlchoholUseComponent} from './alchohol-use/alchohol-use.component';
-import {TobaccoUseComponent} from './tobacco-use/tobacco-use.component';
-import {ArrhythmiaComponent} from './arrhythmia/arrhythmia.component';
 // import {AuthService} from './auth.service';
-
-
 @NgModule({
   declarations: [
     AppComponent
